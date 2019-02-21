5960fa3b997cf70687e134f771f53d0fd9d86051
Beltuv <47578580+Beltuv@users.noreply.github.com>
2019-02-21 11:41:26 -0600

    Update koolbot.js

--- koolbot.js
+++ koolbot.js
@@ -1,5 +1,6 @@
 const commando = require("discord.js-commando");
 const bot = new commando.Client();
+const TOKEN = process.env.token
 
 bot.registry.registerGroup('simple')
 bot.registry.registerGroup('random');
@@ -11,4 +12,4 @@ bot.on('ready', () => {
     bot.user.setActivity("Club Penguin");
   });
 
-bot.login("NTQ3ODA5MzczNzA4MzUzNTU3.D1BxPQ.i1tX8KcL16wUy-_zq4JlhD6EOLM") //TOKEN
+bot.login(TOKEN) //TOKEN
